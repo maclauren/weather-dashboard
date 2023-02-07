feb4abcf24305153a7503a2860f98908a7b691c8
lauren-mac <109279133+lauren-mac@users.noreply.github.com>
2023-02-07 22:00:26 +0000

    display history and loop 5 day forecast

--- script.js
+++ script.js
@@ -37,4 +37,53 @@ $(document).ready(function() {
                   var windSpeed = data.list[0].wind.speed;
                   var date = moment(data.list[0].dt_txt).format('dddd, MMMM Do YYYY, h:mm:ss a');
   
-              }})})})})
+  // displayHistory()
+  var history = JSON.parse(localStorage.getItem("cities")) || []
+  history.push(city)
+  localStorage.setItem("cities", JSON.stringify(history))
+  
+                  $("#today").html("<h2>" + city + ", " + country + "</h2><p>Temperature: " + temperature + "</p><p>Humidity: " + humidity + "</p><p>Wind Speed: " + windSpeed + "</p><p>Date: " + date + "</p>");
+                },
+                error: function(error) {
+                  console.log(error);
+                }
+              });
+              var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+  
+              // ajax to use the lat, lon and apiKey to get weather forecast data
+              $.ajax({
+                  url: apiUrl,
+                  method: "GET"
+              }).then(function(response) {
+          // variables for response data
+                  var data = response.list;
+                  var date = new Date();
+                  var currentDay = date.getDate();
+                  var currentMonth = date.getMonth();
+          console.log(response)
+          var elementIndex = 0
+          // loop to get 5 days after currentday
+                  for (var i = 7; i < data.length; i+=8) {
+                      var forecastDate = new Date(data[i].dt_txt);
+                      var forecastDay = forecastDate.getDate();
+                      var forecastMonth = forecastDate.getMonth();
+          console.log(forecastDay == currentDay + 1 && forecastMonth == currentMonth)
+                      // if (forecastDay == currentDay + 1 && forecastMonth == currentMonth) {
+                          var forecastTemperature = data[i].main.temp;
+                          var forecastHumidity = data[i].main.humidity;
+                          var forecastWindSpeed = data[i].wind.speed;
+                          var forecastDate = moment(forecastDate).format("MM/DD/YYYY");
+          console.log($(`#card${elementIndex}.date`))
+          // add data to cards
+                          $(`#card${elementIndex}-date`).text(forecastDate);
+                          $(`#card${elementIndex}-temp`).text("Temperature: " + forecastTemperature + "°C");
+                          $(`#card${elementIndex}-humidity`).text("Humidity: " + forecastHumidity + "%");
+                          $(`#card${elementIndex}-wind`).text("Wind Speed: " + forecastWindSpeed + "m/s");
+                          elementIndex++
+                      }
+                  // }
+              })
+            })
+          });
+  
+        })
